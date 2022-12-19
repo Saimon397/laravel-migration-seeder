c24452cea3b29fbc57d806e348df3c7598159efb
Saimon397 <simonecarrabba@gmail.com>
2022-12-19 22:29:06 +0100

    work_done100%_farò_style_decente_domani

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,60 +1,47 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layout.app')
+@section('content')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
-<body>
-    <form method="post" action="/filter">
-        @csrf
-        <label for="date">Data di partenza:</label>
-        <input type="date" name="date" value="{{ old('date') }}">
-        <button type="submit">Filtra</button>
-    </form>
-    <table>
-        <thead>
-            <tr>
-                <th>Azienda</th>
-                <th>Stazione di partenza</th>
-                <th>Stazione di arrivo</th>
-                <th>Data di partenza</th>
-                <th>Orario di partenza</th>
-                <th>Orario di arrivo</th>
-                <th>Codice Treno</th>
-                <th>Numero Carrozze</th>
-                <th>In orario</th>
-                <th>Cancellato</th>
+<main>
+<h1 class="mt-5 mb-5 text-danger fw-bold d-flex justify-content-center align-items-center">Trains List:</h1>
+<form class="d-flex justify-content-center align-items-center" method="post" action="/filter">
+    @csrf
+    <label for="date">Data di partenza:</label>
+    <input type="date" name="date" value="{{ old('date') }}">
+    <button type="submit">Filtra</button>
+</form>
+<div class="d-flex justify-content-center align-items-center">
+<table class="mt-5">
+    <thead>
+        <tr>
+            <th class="fs-2 text-danger">Azienda</th>
+            <th class="pe-4 fs-2 text-danger">Stazione di partenza</th>
+            <th class="pe-4 fs-2 text-danger">Stazione di arrivo</th>
+            <th class="pe-4 fs-2 text-danger">Data di partenza</th>
+            <th class="pe-4 fs-2 text-danger">Orario di partenza</th>
+            <th class="pe-4 fs-2 text-danger">Orario di arrivo</th>
+            <th class="pe-4 fs-2 text-danger">Codice Treno</th>
+            <th class="pe-4 fs-2 text-danger">Numero Carrozze</th>
+            <th class="pe-4 fs-2 text-danger">In orario</th>
+            <th class="fs-2 text-danger">Cancellato</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($trains as $train)
+            <tr class="text-center">
+                <td>{{ $train->azienda }}</td>
+                <td>{{ $train->stazione_partenza }}</td>
+                <td>{{ $train->stazione_arrivo }}</td>
+                <td>{{ $train->data_partenza }}</td>
+                <td>{{ $train->orario_partenza }}</td>
+                <td>{{ $train->orario_arrivo }}</td>
+                <td>{{ $train->codice_treno }}</td>
+                <td>{{ $train->numero_carrozze }}</td>
+                <td>{{ $train->in_orario ? 'Sì' : 'No' }}</td>
+                <td>{{ $train->cancellato ? 'Sì' : 'No' }}</td>
             </tr>
-        </thead>
-        <tbody>
-            @foreach ($trains as $train)
-                <tr>
-                    <td>{{ $train->azienda }}</td>
-                    <td>{{ $train->stazione_partenza }}</td>
-                    <td>{{ $train->stazione_arrivo }}</td>
-                    <td>{{ $train->data_partenza }}</td>
-                    <td>{{ $train->orario_partenza }}</td>
-                    <td>{{ $train->orario_arrivo }}</td>
-                    <td>{{ $train->codice_treno }}</td>
-                    <td>{{ $train->numero_carrozze }}</td>
-                    <td>{{ $train->in_orario ? 'Sì' : 'No' }}</td>
-                    <td>{{ $train->cancellato ? 'Sì' : 'No' }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-
-</body>
-
-</html>
+        @endforeach
+    </tbody>
+</table>
+</div>
+</main>
+@endsection
